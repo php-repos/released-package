ae5c24f584ff6c7112162aa88fa02b0e14f5f125
Morteza <morteza@protonmail.com>
2022-09-07 20:40:49 +0200

    initial installation

---
+++ Source/Factories/MyAwesomeFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Saeghe\ReleasedPackage\Factories;
+
+use Saeghe\ComplexPackage\Models\User;
+
+class MyAwesomeFactory
+{
+
+}
---
+++ main-command.php
@@ -0,0 +1,6 @@
+<?php
+
+use Saeghe\ComplexPackage\Controllers\Controller;
+use function Saeghe\ComplexPackage\Helpers\helper1;
+
+$controller = new Controller();
